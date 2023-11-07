83210812c381c2e23f3614aed79558249618af48
Franmeza <ing.meza.francisco@gmail.com>
2023-11-07 17:05:13 -0500

    changed syncronization behavior of the DB

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const mainRouter = require('./routes/index')
+const express = require("express");
+const mainRouter = require("./routes/index");
 const server = express();
-const app = require('./app')
-const { conn } = require('./DB_connection');
+const app = require("./app");
+const { conn } = require("./DB_connection");
 const PORT = 3001;
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-       'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-       'Access-Control-Allow-Methods',
-       'GET, POST, OPTIONS, PUT, DELETE'
-    );
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 server.use(express.json());
- 
-server.use("/rickandmorty", mainRouter)
- 
+
+server.use("/rickandmorty", mainRouter);
+
 conn
-  .sync({ force: true }) //"force:true" => cada vez q se sincronice , borre toda la base de datos y crea las tablas en base a los modelos
+  .sync({ force: false }) //"force:true" => cada vez q se sincronice , borre toda la base de datos y crea las tablas en base a los modelos
   .then(() => {
     server.listen("3001", () => {
       console.log("Server raised in port: ", 3001);
